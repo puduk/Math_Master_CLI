7092392e545dce9fb9ccf49a4b9b5c762250d473
puduk <demirmert986@gmail.com>
2025-04-19 02:59:11 +0200

    Add menu.hpp

---
+++ utils/menu.hpp
@@ -0,0 +1,32 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "Binary.hpp"
+#include "Calculator.hpp"
+#include "Char_Number.hpp"
+#include "Circle.hpp"
+#include "Dice_Roller.hpp"
+#include "Digit_Control.hpp"
+#include "Divider_Shower.hpp"
+#include "Even_Numbers.hpp"
+#include "Exponentiation.hpp"
+#include "Factorial.hpp"
+#include "Gcd.hpp"
+#include "Lcm.hpp"
+#include "Number_Comparing.hpp"
+#include "Number_Guess.hpp"
+#include "Odd_Numbers.hpp"
+#include "Palindrome.hpp"
+#include "Perfect_Number.hpp"
+#include "Prime_Number.hpp"
+#include "Rectangle.hpp"
+#include "Reverse_Number.hpp"
+#include "Rock_Paper_Scissors.hpp"
+#include "Roman_numeral.hpp"
+#include "Square.hpp"
+#include "Square_Root.hpp"
+#include "Triangle.hpp"
+
+void menu();
+
+#endif
